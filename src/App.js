ec552e3d31dbb7dd33ece18cc6efa6841a30c74b
Aditi Sharma <112475970+aditi-sharma-2004@users.noreply.github.com>
2024-06-24 20:31:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import NewsApp from './Components/NewsApp/NewsApp';
+function App() {
+  return (
+    <div className="App">
+      <NewsApp/>
+    </div>
+  );
+}
+
+export default App;
